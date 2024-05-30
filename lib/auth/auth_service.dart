bc80a360715432c622ee5e8ad47bdea35138ce16
Shivam Choudhary <96073230+THESHIVAM007@users.noreply.github.com>
2024-05-30 12:36:48 +0530

    authentication

---
+++ lib/auth/auth_service.dart
@@ -0,0 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_admin/screens/loginpage.dart';
+import 'package:e_commerce_admin/screens/otpscreen.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthRepo {
+  static String verId = "";
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  static void verifyPhoneNumber(BuildContext context, String number) async {
+    await _firebaseAuth.verifyPhoneNumber(
+      phoneNumber: '+91 $number',
+      verificationCompleted: (PhoneAuthCredential credential) {
+        signInWithPhoneNumber(
+            context, credential.verificationId!, credential.smsCode!);
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        if (e.code == 'invalid-phone-number') {
+          print('The provided phone number is not valid.');
+        }
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        verId = verificationId;
+        print("verficationId $verId");
+        Navigator.push(context, MaterialPageRoute(builder: (ctx) {
+          return  OtpScreen(number: number,);
+        }));
+        print("code sent");
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+  }
+
+  static void logoutApp(BuildContext context) async {
+    await _firebaseAuth.signOut();
+    // ignore: use_build_context_synchronously
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => const LoginPage(),
+      ),
+    );
+  }
+
+  static void submitOtp(BuildContext context, String otp) {
+    signInWithPhoneNumber(context, verId, otp);
+  }
+
+  static Future<void> signInWithPhoneNumber(
+      BuildContext context, String verificationId, String smsCode) async {
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: smsCode,
+      );
+      final UserCredential userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
+
+      // Check if the user already exists in Firestore
+      DocumentSnapshot userSnapshot = await _firestore
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .get();
+
+      if (userSnapshot.exists) {
+      //   Navigator.push(
+      //       context, MaterialPageRoute(builder: (context) => const HomePage()));
+      // } else {
+        // User does not exist, navigate to ProfilePage to complete profile
+        // Navigator.push(context, MaterialPageRoute(builder: (context) {
+        //   return ProfilePage(userCredential: userCredential);
+        // }));
+      }
+    } catch (e) {
+      print('Error signing in with phone number: $e');
+    }
+  }
+}
---
+++ lib/screens/otpscreen.dart
@@ -0,0 +1,118 @@
+import 'package:e_commerce_admin/auth/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class OtpScreen extends StatefulWidget {
+  const OtpScreen({super.key, required this.number});
+  final String number;
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  TextEditingController otpController = TextEditingController();
+  bool isVerifying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); // Accessing theme data
+    return Scaffold(
+      backgroundColor: theme.colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.primary,
+        title: Text(
+          'OTP',
+          style: theme.textTheme.titleLarge?.copyWith(
+            // Adjusting the style to match the theme
+            color:
+                theme.colorScheme.surface, // Use the primary color for the text
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            shadows: const [
+              Shadow(
+                color: Colors.black45,
+                offset: Offset(1, 2),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 50),
+            Text(
+              "Verify With OTP Sent to ${widget.number}",
+              style: theme.textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: otpController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    borderSide:
+                        BorderSide(color: theme.colorScheme.primary, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    borderSide: BorderSide(
+                        color: theme.colorScheme.secondary, width: 2),
+                  ),
+                  labelText: "Enter OTP",
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        theme.colorScheme.primary, // Button background color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    foregroundColor:
+                        theme.colorScheme.onPrimary, // Text color on the button
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isVerifying = true;
+                    });
+                    AuthRepo.submitOtp(context, otpController.text);
+                  },
+                  child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: !isVerifying
+                          ? const Text(
+                              "Continue",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            )
+                          : const Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                              ),
+                            )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
